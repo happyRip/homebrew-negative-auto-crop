1e944182811dbe448a9bad505d3fbdaf9e7dd3a4
Aleksander Borowski <aleksander.borowski.01@gmail.com>
2025-12-08 23:55:09 +0100

    Update version to `v0.0.2`

--- Formula/negative-auto-crop.rb
+++ Formula/negative-auto-crop.rb
@@ -1,8 +1,8 @@
 class NegativeAutoCrop < Formula
   desc "Automatic frame detection and cropping for Lightroom Classic film negatives"
   homepage "https://github.com/happyRip/Frame-Detection"
-  url "https://github.com/happyRip/Frame-Detection/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "dbbaa005d7f4fa731056265a72bd0dab5b06596e269ac471e4520460be584c67"
+  url "https://github.com/happyRip/Frame-Detection/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "4742b55a2faf6bf55e942feff79436546aef45884434387a2abb705cbbbf839f"
   license "AGPL-3.0-or-later"
 
   depends_on "python@3.12"
